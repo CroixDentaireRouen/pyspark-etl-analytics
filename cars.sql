a1efa643f0771498ae2a0bfe54f13cfe5bb9e0d6
Croix Dentaire Rouen <38990450+CroixDentaireRouen@users.noreply.github.com>
2020-10-21 15:19:52 +0200

    Small changes to cars_table structure
    vehicleType, gearbox, model, kilometer, fuelType and notRepairedDamage are nullable.

--- cars.sql
+++ cars.sql
@@ -1,18 +1,17 @@
-CREATE TABLE IF NOT EXISTS cars_table
-(
-    name VARCHAR(255) NOT NULL,
-    price integer NOT NULL,
-    abtest VARCHAR(255) NOT NULL,
-	vehicleType VARCHAR(255) NOT NULL,
-	yearOfRegistration VARCHAR(4) NOT NULL,
-	gearbox VARCHAR(255) NOT NULL,
-	powerPS integer NOT NULL,
-	model VARCHAR(255) NOT NULL,
-	kilometer integer NOT NULL,
-	monthOfRegistration integer NOT NULL,
-	fuelType VARCHAR(255) NOT NULL,
-	brand VARCHAR(255) NOT NULL,
-	notRepairedDamage VARCHAR(255) NOT NULL,
-	dateCreated DATE NOT NULL,
-	postalCode VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS cars_table(
+	name VARCHAR(255) NOT NULL,
+        price integer NOT NULL,
+        abtest VARCHAR(255) NOT NULL,
+        vehicleType VARCHAR(255),
+        yearOfRegistration VARCHAR(4) NOT NULL,
+        gearbox VARCHAR(255),
+        powerPS integer NOT NULL,
+        model VARCHAR(255),
+        kilometer integer,
+        monthOfRegistration VARCHAR(255) NOT NULL,
+        fuelType VARCHAR(255),
+        brand VARCHAR(255) NOT NULL,
+        notRepairedDamage VARCHAR(255),
+        dateCreated DATE NOT NULL,
+        postalCode VARCHAR(255) NOT NULL
 );
